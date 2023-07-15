9abc6fbbd138a3750641104082f775db63546e9a
curttree <curttremblay@gmail.com>
2023-07-15 02:53:10 -0400

    Support multiple checkpoints.

--- RecoilRacing/Source/RecoilRacing/Public/CheckPoint.h
+++ RecoilRacing/Source/RecoilRacing/Public/CheckPoint.h
@@ -16,12 +16,25 @@ class RECOILRACING_API ACheckPoint : public ATriggerSphere
 public:
 	UFUNCTION(BlueprintCallable)
 	bool TryActivate();
+
 	UFUNCTION(BlueprintCallable)
 	bool IsActive() 
 	{
 		return bActivated;
 	}
+
+	UFUNCTION(BlueprintCallable)
+	int GetCheckPointIndex()
+	{
+		return CheckPointIndex;
+	}
+
 protected:
 	UPROPERTY(BlueprintReadOnly)
 	bool bActivated;
+
+	// Order for which to prioritize the checkpoint. The active checkpoint with the highest index will be used for respawning.
+	// By no means the best/most efficient solution, but it is a game jam project.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	int CheckPointIndex = 0;
 };
